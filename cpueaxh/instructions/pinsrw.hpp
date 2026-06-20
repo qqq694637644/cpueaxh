96c0a28f83ff893c5504c11753328163fff64a67
gpt-actions-gateway <gpt-actions-gateway@users.noreply.github.com>
2026-06-20 12:32:11 +0800

    Make PINSR headers include AVX helpers

--- cpueaxh/instructions/pinsrw.hpp
+++ cpueaxh/instructions/pinsrw.hpp
@@ -4,6 +4,7 @@
 
 #include "evex.hpp"
 #include "movdq.hpp"
+#include "avx_vex.hpp"
 
 static inline bool is_pinsrw_instruction(const uint8_t* code, int len, int prefix_len) {
     if (!code || prefix_len + 4 >= len) {
