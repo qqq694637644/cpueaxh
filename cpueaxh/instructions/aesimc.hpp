259c5b677ff60052b10f0bf4484ff63e40ff1baa
gpt-actions-gateway <gpt-actions-gateway@users.noreply.github.com>
2026-06-20 10:38:54 +0800

    Make AESIMC header include AES helpers

--- cpueaxh/instructions/aesimc.hpp
+++ cpueaxh/instructions/aesimc.hpp
@@ -2,6 +2,8 @@
 
 // instrusments/aesimc.hpp - AESIMC/VAESIMC implementation
 
+#include "aesenc.hpp"
+
 static XMMRegister apply_aesimc128(XMMRegister source) {
     return aesenc_m128i_to_xmm(_mm_aesimc_si128(aesenc_xmm_to_m128i(source)));
 }
